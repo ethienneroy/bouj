491b2cb86bba9aa115f2820b400d7e5a2ae970a5
eroy <eroy@evivenutrition.com>
2022-01-15 13:57:55 -0500

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '344d7167b639b1be6437750727a4727c'),
+    },
+  },
+});
